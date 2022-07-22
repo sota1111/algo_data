f65e982ef4dcae1e4192b2cd258d593fdb013a19
sota1111 <sota.moro@gmail.com>
2022-07-22 22:22:19 +0900

    bitを用いた組み合わせの全探索

---
+++ algo_code3/algo_code3_5.cpp
@@ -0,0 +1,10 @@
+int main(){
+    // bitの表す部分集合にi番目の要素が含まれる場合
+    int i=1, bit;
+    if(bit &  (1 << i)){
+
+    }else{
+
+    }
+    return(0);
+}
---
+++ algo_code3/algo_code3_6.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    // 入力を受け取る
+    int N, W, bit;
+    cout << "配列数は？" << endl;
+    cin >> N;
+    cout << "目標の和は？" << endl;
+    cin >> W;
+    vector<int> a(N);
+    cout << N << "個の整数を入力ください。" << endl;
+    for(int i=0;i<N;++i) cin >> a[i];
+    //bitは2^N通りの部分集合全体を動きます
+    bool exist = false;
+    for (bit = 0; bit < (1 << N); ++bit){
+        int sum = 0;
+        for (int i=0;i<N;++i){
+            // i番目の要素a[i]が部分集合に含まれているかどうか
+            if (bit & (1 << i)){
+                sum += a[i];
+            }
+        }
+        // sumがWに一致するかどうか
+        if (sum==W) exist = true;
+    }
+    cout << bit << endl;
+    if(exist) cout << "Yes" << endl;
+    else cout << "No" << endl;
+}
