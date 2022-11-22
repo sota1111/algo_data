a7ea56ae3e1f6da474728d6827ec53e75bb02a70
sota1111 <sota.moro@gmail.com>
2022-11-22 20:46:52 +0900

    builを削除

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sota/cpp_make
-# Build directory: /home/sota/cpp_make/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
